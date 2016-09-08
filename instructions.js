d4ec50ff50a3781c3fc09fc89b9fde14e8f49430
sus111 <sgraham535@gmail.com>
2016-09-08 16:11:35 +0100

    instructions tests

--- instructions.js
+++ instructions.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
 var typed = "";
-var currentLetter = '';
-var index = 0;
+var instructions = [];
 
   $(document).on("keypress", "#typed-text", function(e) {
        if (e.which == 13) {
          typed += $('#typed-text').val();
+         instructions += $('#typed-text').val();
          document.getElementById('typed-text').value = "";
          $('#entered-text').html(typed);
-
        }
   });
 
