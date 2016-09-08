2f2351ea6b7190d71c97ae99d1d201f5d82cfd3d
sus111 <sgraham535@gmail.com>
2016-09-08 18:48:52 +0100

    merge stuff

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var displayChar = document.getElementById('character');
 
 
 document.addEventListener("click", function() {
-  character.moveLeft(1);
+  character.moveRight(1);
   if(character.position[0] <= 0){
     character.position[0] = 0;
   }
