19c6b6eaa5dc78112eb723982c284b3eb1a2526e
sus111 <sgraham535@gmail.com>
2016-09-08 14:08:31 +0100

    add instructions entry

---
+++ spec/featureSpec/featureSpecHelper.js
@@ -0,0 +1,16 @@
+var Browser = require('zombie');
+var assert = require('assert');
+var chai = require('chai');
+var expect  = chai.expect;
+var assert = chai.assert;
+
+describe('home page', function() {
+  beforeEach(function() {
+    this.browser = new Browser({ site: 'http://localhost:8080' });
+  });
+
+  beforeEach(function(done) {
+    this.browser.visit('/', done);
+  });
+
+});
