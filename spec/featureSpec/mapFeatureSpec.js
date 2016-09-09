49a7104b817a5df8c08848f277c7a6b51f3ce4e2
Michael <mjosephmiller.uk@gmail.com>
2016-09-09 14:02:34 +0100

    emoji tests

--- spec/featureSpec/mapFeatureSpec.js
+++ spec/featureSpec/mapFeatureSpec.js
@@ -5,25 +5,27 @@ var expect  = chai.expect;
 var assert = chai.assert;
 
 describe('home page', function() {
-  beforeEach(function() {
-    this.browser = new Browser({ site: 'http://localhost:8080' });
-  });
+ beforeEach(function() {
+   this.browser = new Browser({ site: 'http://localhost:8080' });
+ });
 
-  beforeEach(function(done) {
-    this.browser.visit('/', done);
-  });
+ beforeEach(function(done) {
+   this.browser.visit('/', done);
+ });
 
-  it('should load', function(){
-    assert.ok(this.browser.success);
-  });
+ it('should load', function(){
+   assert.ok(this.browser.success);
+ });
 
-  it('loads the grid', function(){
-    this.browser.assert.element('#container');
-  });
+ it('loads the grid', function(){
+   this.browser.assert.element('#container');
+ });
 
-  it('loads the grid with coloured tiles', function(){
-    this.browser.assert.attribute('.dirt');
-    this.browser.assert.attribute('.grass');
-  });
-
+ it('loads the grid with coloured tiles', function(){
+   // this.browser.assert.attribute('.dirt');
+   this.browser.assert.attribute('.grass');
+ });
+ it('loads the 1s with an emoji', function(){
+   this.browser.assert.attribute('#a11', "💍");
+ });
 });
