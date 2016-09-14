80b07ac355c12ec0399648309c04fbf29887ea32
lukew244 <luke.webster86@gmail.com>
2016-09-14 23:05:49 +0100

    Updates SpecRunner and Instructions unit test

--- spec/unitSpec/instructionsSpec.js
+++ spec/unitSpec/instructionsSpec.js
@@ -1,21 +1,25 @@
 describe ("instructions", function() {
   var instructions = new Instructions();
 
+  it("should keep track of instructions added", function() {
+    instructions.append("moveUp(1)");
+    expect(instructions.list).toEqual(['moveUp(1)']);
+  });
+
+
   it("should add instructions to the typed variable", function() {
+    instructions.typed = "";
     instructions.append("moveUp");
     expect(instructions.typed).toEqual("moveUp\n");
   });
 
-<<<<<<< HEAD
-  it("should add instructions to the instructions array", function() {
-    instructions.addInstructions("moveUp");
-    expect(instructions.instructionsArr).toContain("moveUp");
-  });
+
+
 //
 // describe 'process'
 // beforeEach
 //   var insturctionsArr = "moveRight(2)", "moveLeft(4)"]);
-// 
+//
 //   it('should parse instructions and return a function', function(){
 //     instructions.process()
 //   });
@@ -23,13 +27,13 @@ describe ("instructions", function() {
 //   it('should parse instructions and return a number', function(){
 //
 //   });
-=======
+
   //taken out as method has moved, the above test needs refactoring
 
   // it("should add instructions to the instructions array", function() {
   //   instructions.addInstructions("moveUp");
   //   expect(instructions.instructionsArr).toContain("moveUp");
   // });
->>>>>>> a32bad1d048ae892d3c3a76aea930ca427768a59
+
 
 });
