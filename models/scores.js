d0c3d3b3fdaa1ad9451a4bca4e02bc2987505c26
lukew244 <luke.webster86@gmail.com>
2016-09-14 19:03:13 +0100

    refactors scores

--- models/scores.js
+++ models/scores.js
@@ -1,46 +1,28 @@
 (function(exports) {
 
   function Scores() {
-    this.list = [];
+    this.scoreServerUrl = "http://localhost:9292/scores";
   }
 
-
   Scores.prototype.save = function(name, score, level){
-    var httprequest = new XMLHttpRequest();
-    var jsonName = JSON.stringify(name);
-    var jsonScore = JSON.stringify(score);
-    var jsonLevel = JSON.stringify(level);
-    httprequest.open("POST", "http://localhost:9292/scores?name=" + jsonName + "&score=" + jsonScore + "&level=" + jsonLevel);
-    httprequest.send(jsonScore);
+    var request = new XMLHttpRequest();
+    name = JSON.stringify(name);
+    score = JSON.stringify(score);
+    level = JSON.stringify(level);
+    request.open("POST", this.scoreServerUrl + "?name=" + name +
+    "&score=" + score + "&level=" + level);
+    request.send();
   };
-
 
   Scores.prototype.retrieve = function() {
-  var httprequest = new XMLHttpRequest();
-  var scores = this;
-  httprequest.onreadystatechange = function() {
-    if (httprequest.readyState == 4 && httprequest.status == 200) {
-    // scores.list.push(JSON.parse(httprequest.responseText));
-    parsed = JSON.parse(httprequest.responseText);
-    scores.sortByPoints(httprequest.responseText);
-    scores.list.push(JSON.parse(httprequest.responseText));
-    // displayLeaderboard(scores.list);
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+    prepareLeaderboard(JSON.parse(request.responseText));
     }
   };
-    httprequest.open("GET", "http://localhost:9292/scores");
-    httprequest.send();
-    return this.list;
-};
-
-Scores.prototype.sortByPoints = function(json) {
-  formattedScores = JSON.parse(json);
-  sorted = formattedScores.sort(function(a, b) {
-    return parseInt(b.score) - parseInt(a.score);
-  });
-  displayLeaderboard(sorted);
-  // sorted.forEach(function(item) {
-  //   console.log(item);
-  // });
+    request.open("GET", this.scoreServerUrl);
+    request.send();
 };
 
   exports.Scores = Scores;
