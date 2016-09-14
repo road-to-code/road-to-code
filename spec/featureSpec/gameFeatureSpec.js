fc03bdc5f8887d4daa0918a2cfd318b6b3cc7ac6
sus111 <sgraham535@gmail.com>
2016-09-14 14:48:53 +0100

    passing tests

--- spec/featureSpec/gameFeatureSpec.js
+++ spec/featureSpec/gameFeatureSpec.js
@@ -17,7 +17,6 @@ describe('home page', function() {
     this.browser.assert.element('#runInstructions');
   });
 
-
   it('should remove emojis on collision', function(){
     var browser = this.browser;
     this.browser.fill('#typed-text', "moveRight(2)");
@@ -27,36 +26,6 @@ describe('home page', function() {
         browser.assert.text("#a01", "");
         browser.assert.text("#a02", "");
       });
-  });
-
-  xit('should move the character 2 spaces down', function(){
-    var browser = this.browser;
-    this.browser.fill('#typed-text', "moveDown(9)");
-    this.browser.pressButton('Add Instruction');
-    this.browser.pressButton('Run');
-    this.browser.wait().then( function() {
-    browser.assert.style('#character.style', "left");
-    });
-  });
-
-  xit('should respond to multiple instructions', function(){
-    var browser = this.browser;
-    this.browser.fill('#typed-text', "moveRight(4)");
-    this.browser.pressButton('Add Instruction');
-    this.browser.fill('#typed-text', "moveDown(4)");
-    this.browser.pressButton('Add Instruction');
-    this.browser.fill('#typed-text', "moveUp(2)");
-    this.browser.pressButton('Add Instruction');
-    this.browser.fill('#typed-text', "moveLeft(2)");
-    this.browser.pressButton('Add Instruction');
-    this.browser.pressButton('Run');
-    this.browser.wait().then( function() {
-      browser.assert.text('#points-score', '310');
-      console.log(browser.body.innerHTML);
-
-      // this.browser.assert.style('#character', 'left', "100px");
-      // this.browser.assert.style('#character', 'top', "100px");
-    });
   });
 
   it('should increase the score as more emojis are picked up', function(){
@@ -69,10 +38,10 @@ describe('home page', function() {
     });
   });
 
+  it('should turn the path behind a darker green', function(){
+    this.browser.fill('#typed-text', "moveRight(4)");
+    this.browser.pressButton('Add Instruction');
+    this.browser.pressButton('Run');
+    this.browser.assert.attribute('#a04', 'dirt');
   });
-  // it('should turn the path behind a darker green', function(){
-  //   this.browser.fill('#typed-text', "moveRight(4)");
-  //   this.browser.pressButton('Add Instruction');
-  //   this.browser.pressButton('Run');
-  //   this.browser.assert.text('#a04', 'dirt');
-  // });
+});
